7136e12d3a68a5a3c0d8570cfd28ecbb8533f55d
AsterotthCR <sheancordero97@gmail.com>
2024-04-17 22:48:13 -0500

    Tailwind install

--- Frontend/gg.store.frontend/src/app/home/home.page.ts
+++ Frontend/gg.store.frontend/src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonAvatar,
+  IonList,
+  IonLabel,
+  IonIcon, IonItem
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonAvatar, IonList, IonLabel,IonIcon,IonItem],
 })
 export class HomePage {
   constructor() {}
